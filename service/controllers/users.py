69c865aa91f7ac75b54c8605526442ea8ee39c1d
ermiry <erick.salas@ermiry.com>
2021-04-25 13:24:41 -0500

    Added controller methods to handle login

--- service/controllers/users.py
+++ service/controllers/users.py
@@ -1,10 +1,19 @@
+import datetime
+
 from flask import request
+import jwt
 
 from errors import *
 import runtime
 import todo
 
 from models.user import *
+
+app = None
+
+def todo_users_init (flask_app):
+	global app
+	app = flask_app
 
 def todo_user_get_by_email (email):
 	found = user_get_by_email (email)
@@ -55,3 +64,42 @@ def todo_users_register (request):
 		error = TODO_ERROR_MISSING_VALUES
 
 	return error
+
+def todo_user_generate_token (user):
+	token = jwt.encode (
+		{
+			"iat": datetime.datetime.utcnow (),
+			"id": user.id,
+			"name": user.name,
+			"username": user.username,
+			"role": "common"
+		},
+		app.config['PRIV_KEY'],
+		app.config["JWT_ALGORITHM"]
+	)
+
+	return jsonify ({'token': token})
+
+def todo_users_login (request):
+	error = TODO_ERROR_NONE
+	user = None
+
+	if (request.json is not None):
+		email = request.json['email']
+		password = request.json['password']
+
+		if (email and password):
+			user = todo_user_get_by_email (email)
+			if todo.RUNTIME == runtime.RUNTIME_TYPE_DEVELOPMENT:
+				print (user)
+			
+			if (user.password != password):
+				error = TODO_ERROR_BAD_REQUEST
+
+		else:
+			error = TODO_ERROR_MISSING_VALUES
+
+	else:
+		error = TODO_ERROR_MISSING_VALUES
+
+	return error, user
