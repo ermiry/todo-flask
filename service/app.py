dff1d8302a20404fb7974058136da03f6d449f0c
ermiry <erick.salas@ermiry.com>
2021-04-25 15:10:23 -0500

    Added JWT keys configuration in main source

--- service/app.py
+++ service/app.py
@@ -4,11 +4,21 @@ import db
 import todo
 import version
 
+from controllers.service import todo_service_init
+from controllers.users import todo_users_init
+
 from routes.items import items
 from routes.service import service
 from routes.users import users
 
 app = Flask (__name__)
+
+app.config["JWT_ALGORITHM"] = "RS256"
+with open (todo.PRIV_KEY, "r") as file:
+    app.config["PRIV_KEY"] = file.read ()
+with open (todo.PUB_KEY, "r") as file:
+    app.config["PUB_KEY"] = file.read ()
+
 app.register_blueprint (items)
 app.register_blueprint (service)
 app.register_blueprint (users)
@@ -16,7 +26,11 @@ app.register_blueprint (users)
 if __name__ == '__main__':
 	version.todo_version_print_full ()
 
-	todo.todo_init ()
-	# db.todo_mongo_init ()
+	todo.todo_config ()
+	
+	db.todo_mongo_init ()
+
+	todo_service_init (app)
+	todo_users_init (app)
 
 	app.run (debug=True, host="0.0.0.0", port=todo.PORT)
